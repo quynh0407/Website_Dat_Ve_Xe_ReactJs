2c2620b51541e185a1802e759187b621ca8aa8b2
phucle1011 <phuc628780@gmail.com>
2025-03-25 17:31:17 +0700

    Trang về chúng tôi update
    (cherry picked from commit cdc4d4e4c6b92b7a052ef0d395018b7554428ddd)

--- src/pages/client/About-us/index.jsx
+++ src/pages/client/About-us/index.jsx
@@ -1,6 +1,6 @@
 function AboutUs() {
     return(
-    <main className="home mx-auto w-full md:w-[80%] mt-[11%] px-4" >
+    <main className="home mx-auto w-full md:w-[80%] mt-[11%] px-4" id="home">
         <div className="bg-white rounded-lg p-4 mb-3">
             <div className="font-sans text-gray-800">
                 <div className="container mx-auto px-4 py-8">
