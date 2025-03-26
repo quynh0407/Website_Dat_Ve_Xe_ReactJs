7c1bf8a30434033cb9bae62d5ea385c44e17a741
quynh0407 <quynh0939662390@gmail.com>
2025-03-26 22:11:55 +0700

    Merge branch 'PhuongQuynh' of https://github.com/quynh0407/Website_Dat_Ve_Xe_ReactJs into PhuongQuynh

--- src/pages/client/Bus/index.jsx
+++ src/pages/client/Bus/index.jsx
@@ -6,6 +6,12 @@ function Bus (){
                     <div className="flex p-4 justify-between">
                         <span className="font-bold text-[20px]">Bộ lọc tìm kiếm </span>
                         <span className="text-[18px] text-orange-600"><a href="">Bỏ lọc</a></span>
+        <main className="mx-auto w-full md:w-[80%] flex pb-3 " id="busDetail">
+            <div className="w-[40%] bg-white p-4 rounded-lg">
+                <div className="w-full">
+                    <div className="flex p-4 justify-between">
+                        <span className="font-bold text-[20px]">Bộ lọc tìm kiếm </span>
+                        <span className="text-[18px] text-orange-600"><a href="">Bỏ lọc</a></span>
                        
                     </div>
                     <div className>
@@ -13,26 +19,42 @@ function Bus (){
                         <div className="grid grid-cols-1 pl-4">
                             <div className="flex pt-2">
                                 <div className><input className="ant checkbox input mt-2"
+                    <div className>
+                        <span className="text-[19px] p-4">Giờ đi</span>
+                        <div className="grid grid-cols-1 pl-4">
+                            <div className="flex pt-2">
+                                <div className><input className="ant checkbox input mt-2"
                                         type="checkbox" name id/></div>
+                                <div className="ml-2 text-[18px]">Sáng sớm 00:00 -
                                 <div className="ml-2 text-[18px]">Sáng sớm 00:00 -
                                     06:00(0)</div>
                             </div>
                             <div className="flex pt-2">
                                 <div className><input className="ant checkbox input mt-2"
+                            <div className="flex pt-2">
+                                <div className><input className="ant checkbox input mt-2"
                                         type="checkbox" name id/></div>
+                                <div className="ml-2 text-[18px]">Buổi sáng 06:00 - 12:00
                                 <div className="ml-2 text-[18px]">Buổi sáng 06:00 - 12:00
                                     (0)</div>
                             </div>
                             <div className="flex pt-2">
                                 <div className><input className="ant checkbox input mt-2"
+                            <div className="flex pt-2">
+                                <div className><input className="ant checkbox input mt-2"
                                         type="checkbox" name id/></div>
+                                <div className="ml-2 text-[18px]">Buổi chiều 12:00 - 18:00
                                 <div className="ml-2 text-[18px]">Buổi chiều 12:00 - 18:00
                                     (0)</div>
                             </div>
                             <div className="flex pt-2 ">
                                 <div className="">
                                     <input className="ant checkbox input mt-2"
+                            <div className="flex pt-2 ">
+                                <div className="">
+                                    <input className="ant checkbox input mt-2"
                                         type="checkbox" name id/></div>
+                                <div className="ml-2 text-[18px]">Buổi tối 18:00 - 24:00
                                 <div className="ml-2 text-[18px]">Buổi tối 18:00 - 24:00
                                     (0)</div>
                             </div>
@@ -41,16 +63,21 @@ function Bus (){
 
                     <div className="p-4"><span className="text-[19px]">Loại xe</span><div
                             className="mt-4 flex flex-wrap gap-2">
+                    <div className="p-4"><span className="text-[19px]">Loại xe</span><div
+                            className="mt-4 flex flex-wrap gap-2">
                             <a href>
                                 <div
+                                    className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Ghế
                                     className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Ghế
                                 </div></a>
                             <a href>
                                 <div
                                     className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Giường
+                                    className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Giường
                                 </div></a>
                             <a href>
                                 <div
+                                    className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Limousine
                                     className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE] bg-[#FEF6F3] text-orange">Limousine
                                 </div></a>
                         </div>
@@ -58,14 +85,19 @@ function Bus (){
 
                     <div className="p-4 pb-6">
                         <span className="text-[19px]">Tầng</span>
+                    <div className="p-4 pb-6">
+                        <span className="text-[19px]">Tầng</span>
                         <div
+                            className="mt-4 flex flex-wrap gap-2">
                             className="mt-4 flex flex-wrap gap-2">
                             <a href>
                                 <div
                                     className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE]">Tầng
+                                    className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE]">Tầng
                                     trên</div></a>
                             <a href>
                                 <div
+                                    className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE]">Tầng
                                     className="cursor-pointer rounded-md border bg-white py-1 px-3 text-[18px] font-normal border-[#FCDACE]">Tầng
                                     dưới</div></a>
                         </div>
@@ -111,8 +143,11 @@ function Bus (){
                         </div>
                         <div className="grid grid-cols-1 justify-between">
                             <div className>
+                        <div className="grid grid-cols-1 justify-between">
+                            <div className>
                             </div>
                             <span
+                                className="p-2 text-xl pt-15 font-bold text-orange-600 ">200.0000đ</span>
                                 className="p-2 text-xl pt-15 font-bold text-orange-600 ">200.0000đ</span>
                         </div>
                     </div>
@@ -122,11 +157,16 @@ function Bus (){
                             <div className="flex">
                                 <div
                                     className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
+                                    className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
                                 <span
+                                    className="p-2 text-[18px] text-gray-700">Limousine</span>
                                     className="p-2 text-[18px] text-gray-700">Limousine</span>
                             </div>
                             <div className="flex">
+                            <div className="flex">
                                 <div
+                                    className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
+                                <span className="p-2 text-[18px] text-green-700">32
                                     className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
                                 <span className="p-2 text-[18px] text-green-700">32
                                     chỗ trống</span>
@@ -134,8 +174,10 @@ function Bus (){
                         </div>
                         <button
                             className="p-2 pl-6 pr-6 bg-orange-500 rounded-[30px] text-white"><a href="/bookingTicket">Chọn
+                            className="p-2 pl-6 pr-6 bg-orange-500 rounded-[30px] text-white"><a href="/bookingTicket">Chọn
                                 chuyến</a></button>
                     </div>
+                    <div className></div>
                     <div className></div>
                 </div>
 
@@ -176,8 +218,11 @@ function Bus (){
                         </div>
                         <div className="grid grid-cols-1 justify-between">
                             <div className>
+                        <div className="grid grid-cols-1 justify-between">
+                            <div className>
                             </div>
                             <span
+                                className="p-2 text-xl pt-15 font-bold text-orange-600 ">200.0000đ</span>
                                 className="p-2 text-xl pt-15 font-bold text-orange-600 ">200.0000đ</span>
                         </div>
                     </div>
@@ -187,11 +232,16 @@ function Bus (){
                             <div className="flex">
                                 <div
                                     className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
+                                    className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
                                 <span
+                                    className="p-2 text-[18px] text-gray-700">Limousine</span>
                                     className="p-2 text-[18px] text-gray-700">Limousine</span>
                             </div>
                             <div className="flex">
+                            <div className="flex">
                                 <div
+                                    className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
+                                <span className="p-2 text-[18px] text-green-700">32
                                     className="h-[6px] w-[6px] rounded-full bg-[#C8CCD3] mt-5"></div>
                                 <span className="p-2 text-[18px] text-green-700">32
                                     chỗ trống</span>
@@ -201,6 +251,7 @@ function Bus (){
                             className="p-2 pl-6 pr-6 bg-orange-500 rounded-[30px] text-white"><a href="/bookingTicket">Chọn
                                 chuyến</a></button>
                     </div>
+                    <div className></div>
                     <div className></div>
                 </div>
             </div>
