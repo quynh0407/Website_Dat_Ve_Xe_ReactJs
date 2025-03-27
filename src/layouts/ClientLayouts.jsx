ccb3200cf3662c9e81ab676d1db26ec2786fb5b1
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-03-27 21:01:19 +0700

    [ FIX ] - client.main.js

--- src/layouts/ClientLayouts.jsx
+++ src/layouts/ClientLayouts.jsx
@@ -1,13 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
+import React, { useEffect } from "react";
 
 import Header from "../components/client/Header";
 import Footer from "../components/client/Footer";
 
 import  "../styles/client/css/index.css";
-import "../styles/client/js/main.ts"
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'intersection-observer';
+import { initCommentSlider, initSwiper,initPageVisibility, initMenu, initBusAnimation } from "../styles/client/js/main.js";
+
 
 const ClientLayout = () => {
+  useEffect(() => {
+    initCommentSlider();
+    initSwiper();
+    initMenu();
+    initBusAnimation();
+    initPageVisibility();
+  }, []);
+  
   return (
     <>
         <Header/>
