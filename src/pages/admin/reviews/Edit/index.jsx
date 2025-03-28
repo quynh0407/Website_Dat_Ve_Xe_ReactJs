53148e84c29353559bb1e6a5a524289bbe4134ee
quynh0407 <quynh0939662390@gmail.com>
2025-03-28 23:07:35 +0700

    [ ADMIN-FE ] - note7.getAll-edit.review
    (cherry picked from commit 717b9abee1593c1b6367cf6b30abb3680a14b9df)

--- src/pages/admin/reviews/Edit/index.jsx
+++ src/pages/admin/reviews/Edit/index.jsx
@@ -1,6 +1,129 @@
-function reviewEdit(){
-    return(
-        <>This is reviewEdit</>
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const reviewData = [
+    {
+        id: "1",
+        userID: "Nguyễn Văn A",
+        tripID: "502",
+        rating: "3",
+        comment: "120",
+        createAt: "28/03/2025",
+        status: 1,
+    },
+    {
+        id: "2",
+        userID: "Nguyễn Văn B",
+        tripID: "501",
+        rating: "5",
+        comment: "1800000",
+        createAt: "28/03/2025",
+        status: 0,
+    },
+];
+function ReviewEdit() {
+
+    const { id } = useParams();
+    const [formData, setFormData] = useState({ userID: "", tripID: "", rating: "", comment: "", createAt: "", status: "" });
+
+    useEffect(() => {
+        const reviewToEdit = reviewData.find(review => review.id === id);
+        if (reviewToEdit) {
+            setFormData(reviewToEdit);
+        }
+    }, [id]);
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="container mx-auto p-4">
+            <div className="bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto">
+                <h3 className="text-2xl font-bold mb-4">Chỉnh sửa đánh giá</h3>
+                <form onSubmit={handleSubmit} className="p-4 border rounded-md shadow-lg">
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Người dùng</label>
+                        <input
+                            type="text"
+                            name="userID"
+                            value={formData.userID}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded"
+                            disabled
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Mã chuyến xe</label>
+                        <input
+                            type="text"
+                            name="tripID"
+                            value={formData.tripID}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded"
+                            disabled
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Đánh giá</label>
+                        <div className="flex items-center gap-2">
+                            <input
+                                type="text"
+                                name="rating"
+                                value={formData.rating}
+                                onChange={handleChange}
+                                className="w-[35px] p-2 rounded"
+                                disabled
+                            />
+                            <span>
+                                {Array.from({ length: formData.rating }, (_, i) => <span key={i}>⭐</span>)}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Bình luận</label>
+                        <input
+                            type="text"
+                            name="comment"
+                            value={formData.comment}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded"
+                            disabled
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Ngày đăng</label>
+                        <input
+                            type="text"
+                            name="createAt"
+                            value={formData.createAt}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded"
+                            disabled
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Trạng thái</label>
+                        <select
+                            type="text"
+                            name="status"
+                            value={formData.status}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded">
+                            <option value="1">Hiển thị</option>
+                            <option value="0">Ẩn</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="px-4 py-2 bg-[#073272] text-white rounded">Cập nhật đánh giá </button>
+                </form>
+            </div>
+        </div>
     )
 }
-export default reviewEdit;
+export default ReviewEdit;
--- src/pages/admin/reviews/getAll/index.jsx
+++ src/pages/admin/reviews/getAll/index.jsx
@@ -1,10 +1,76 @@
-function ReviewGetAll(){
-    return(
-        <div className="container-fluid">
-        <div className="row">
-            <p>This is review Get All</p>
+import { Link } from "react-router-dom";
+
+const reviewData = [
+    {
+        id: "1",
+        userID: "Nguyễn Văn A",
+        tripID: "502",
+        rating: "3",
+        comment: "120",
+        createAt: "28/03/2025",
+        status: "Hiển thị",
+    },
+    {
+        id: "2",
+        userID: "Nguyễn Văn B",
+        tripID: "501",
+        rating: "5",
+        comment: "1800000",
+        createAt: "28/03/2025",
+        status: "Ẩn",
+    },
+];
+
+function ReviewGetAll() {
+    return (
+        <div className="container mx-auto p-2">
+            <div className="bg-white p-4 shadow rounded-md">
+                <table className="w-full border-collapse border border-gray-300 mt-4">
+                    <thead>
+                        <tr className="bg-gray-200">
+                            <th className="p-2 border">Người dùng</th>
+                            <th className="p-2 border">Mã chuyến</th>
+                            <th className="p-2 border">Đánh giá</th>
+                            <th className="p-2 border">Bình luận</th>
+                            <th className="p-2 border">Ngày đăng</th>
+                            <th className="p-2 border">Trạng thái</th>
+                            <th className="p-2 border">Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reviewData.map((review) => (
+                            <tr key={review.id} className="border-b">
+                                <td className="p-2 border">{review.userID}</td>
+                                <td className="p-2 border">{review.tripID}</td>
+                                <td className="p-2 border"><span className="pr-1">★</span>{review.rating}/5</td>
+                                <td className="p-2 border">{review.comment}</td>
+                                <td className="p-2 border">{review.createAt}</td>
+                                <td className="p-2 border">
+                                    <span className={`py-1 px-4 rounded-full 
+                                        ${review.status === "Hiển thị" ? "bg-gradient-to-b from-cyan-100 to-indigo-200 text-blue-950" 
+                                                                        : "bg-gradient-to-b from-yellow-100 to-rose-300 text-red-950" }`}
+                                    >
+                                    {review.status}
+                                    </span>
+                                </td>
+                                <td className="p-2 border flex gap-2">
+                                    <Link
+                                        to={`/admin/review/edit/${review.id}`}
+                                        className="bg-yellow-500 text-white py-2 px-3 rounded"
+                                    >
+                                        <i className="fa-solid fa-pen-to-square text-md"></i>
+                                    </Link>
+                                    <button className="bg-red-500 text-white py-2 px-3 rounded">
+                                        <i className="fa-solid fa-trash text-md"></i>
+                                    </button>
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     )
 }
 export default ReviewGetAll;
