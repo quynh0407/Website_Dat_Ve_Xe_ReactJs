242a33d8e19642ddc5252eb881cf3a960fd8709f
phucle1011 <phuc628780@gmail.com>
2025-04-01 18:47:01 +0700

    [ ADMIN - FE ] - note8.VALIDATION- driver.bus.routes
    (cherry picked from commit 16080f45845b67b34f26bd04d2bdd238c731d8e2)

--- src/pages/admin/bus/Create/index.jsx
+++ src/pages/admin/bus/Create/index.jsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form";
+
 function BusCreate() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data)
+
     return (
         <div className="container mx-auto p-4">
             <div className="bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto">
@@ -9,37 +20,52 @@ function BusCreate() {
                         <input
                             type="text"
                             className="w-full p-2 border rounded"
-                            placeholder="Vui lòng nhập biển số"
+                            placeholder="Vui lòng nhập biển số" id="plateNumber"
                             required
+                            {...register("plateNumber", {
+                                required: "Biển số không được để trống",
+                            })}
                         />
+                        {errors.plateNumber && <span className="text-danger">{errors.plateNumber.message}</span>}
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium mb-2">Loại xe</label>
-                        <select className="w-full p-2 border rounded" required>
+                        <select className="w-full p-2 border rounded" id="busTypeID " required
+                            {...register("busTypeID", {
+                                required: "Loại xe không được để trống",
+                            })}>
                             <option value="">Chọn loại xe</option>
                             <option value="1">Giường nằm</option>
                             <option value="2">Ghế ngồi</option>
                         </select>
+                        {errors.busTypeID && <span className="text-danger">{errors.busTypeID.message}</span>}
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium mb-2">Tài xế</label>
                         <input
                             type="text"
                             className="w-full p-2 border rounded"
-                            placeholder="Vui lòng nhập tên tài xế"
+                            placeholder="Vui lòng nhập tên tài xế" id="driverId"
                             required
-                        />
+                            {...register("driverId", {
+                                required: "Tài xế không được để trống",
+                            })} />
+                        {errors.driverId && <span className="text-danger">{errors.driverId.message}</span>}
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium mb-2">Trạng thái</label>
-                        <select className="w-full p-2 border rounded" required>
+                        <select className="w-full p-2 border rounded" id="status" required
+                            {...register("status", {
+                                required: "Trạng thái không được để trống",
+                            })}>
                             <option value="">Chọn trạng thái</option>
                             <option value="active">Hoạt động</option>
                             <option value="maintenance">Bảo trì</option>
                             <option value="repairing">Đang sửa chữa</option>
                         </select>
+                        {errors.status && <span className="text-danger">{errors.status.message}</span>}
                     </div>
-                    <button type="submit" className="px-4 py-2 bg-[#073272] text-white rounded">
+                    <button type="submit" onClick={handleSubmit(onSubmit)} className="px-4 py-2 bg-[#073272] text-white rounded">
                         Thêm xe
                     </button>
                 </form>
