67ac6e95e83ccb2c4ed702053dde00bd08369f02
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-04-01 15:59:02 +0700

    [ ADMIN - FE ] - note7.VALIDATION.busRoute

--- src/pages/admin/busRoutes/Edit/index.jsx
+++ src/pages/admin/busRoutes/Edit/index.jsx
@@ -1,92 +1,123 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const busRouteData = [
-    { id: "1", route: "Hà Nội-TP. HCM",departure:"13h30", arrival: "23h30", price: "120" },
-    { id: "2", route: "Hà Nội-TP. HCM",departure:"13h30", arrival: "23h30", price: "1800000" },
+    { id: "1", route: "route1", startTime: "2023-10-10T13:30", endTime: "2023-10-10T23:30", ticketPrice: "120000" },
+    { id: "2", route: "route2", startTime: "2023-10-10T13:30", endTime: "2023-10-10T23:30", ticketPrice: "1800000" },
 ];
 
-function BusRoutesEdit(){
+function BusRoutesEdit() {
+    const { 
+        register,
+        handleSubmit,
+        setValue,
+        trigger, 
+        formState: { errors }
+    } = useForm({
+        mode: "onChange"
+    });
+    
     const { id } = useParams();
-        const [formData, setFormData] = useState({ route: "", departure: "", arrival: "", price: "" });
-    
-        useEffect(() => {
-            const busrouteToEdit = busRouteData.find(busroute => busroute.id === id);
-            if (busrouteToEdit) {
-                setFormData(busrouteToEdit);
-            }
-        }, [id]);
-    
-        const handleChange = (e) => {
-            setFormData({
-                ...formData,
-                [e.target.name]: e.target.value,
-            });
-        };
-    
-        const handleSubmit = (e) => {
-            e.preventDefault();
-        };
+    const [formData, setFormData] = useState(null);
 
-        const formatPrice = (price) => {
-            return price.toLocaleString('vi-VN') + ' đ';
-        };
+    useEffect(() => {
+        const busRouteToEdit = busRouteData.find(busRoute => busRoute.id === id);
+        if (busRouteToEdit) {
+            setFormData(busRouteToEdit);
+            Object.keys(busRouteToEdit).forEach(key => setValue(key, busRouteToEdit[key]));
+        }
+    }, [id, setValue]);
+
+    const onSubmit = (data) => console.log("dulieu cap naht", data);
+
+    const handleChange = async (fieldName) => {
+        await trigger(fieldName); 
     
-        return (
-            <div className="container mx-auto p-4">
-                    <div className="bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto">
-                <h3 className="text-2xl font-bold mb-4">Chỉnh sửa tuyến xe</h3>
-                <form onSubmit={handleSubmit} className="p-4 border rounded-md shadow-lg">
+    };
+
+ 
+
+    return (
+        <div className="container mx-auto p-4">
+            <div className="bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto">
+                <h3 className="text-2xl font-bold mb-2">Chỉnh sửa tuyến xe</h3>
+                <form onSubmit={handleSubmit(onSubmit)} className="p-4 border rounded-md shadow-lg">
                     <div className="mb-4">
-                        <label className="block text-sm font-medium">Tuyến đường</label>
-                        <input
-                            type="text"
-                            name="route"
-                            value={formData.route}
-                            onChange={handleChange}
-                            className="w-full p-2 border rounded"
-                            required
-                        />
+                        <label className="block text-sm font-medium mb-2">Tuyến đường</label>
+                        <select
+                            className={`w-full p-2 border rounded ${errors.route ? '!border-red-500' : 'border-gray-300'}`}
+                            {...register("route", { required: "Vui lòng chọn tuyến đường" })}
+                            onChange={() => handleChange("route")}
+                        >
+                            <option value="">Chọn tuyến đường</option>
+                            <option value="route1">Tuyến A</option>
+                            <option value="route2">Tuyến B</option>
+                            <option value="route3">Tuyến C</option>
+                        </select>
+                        {errors.route && <p className="text-red-700">{errors.route.message}</p>}
                     </div>
+
                     <div className="mb-4">
-                        <label className="block text-sm font-medium">Giờ bắt đầu</label>
+                        <label className="block text-sm font-medium mb-2">Giờ bắt đầu</label>
                         <input
-                            type="timestamp"
-                            name="departure"
-                            value={formData.departure}
-                            onChange={handleChange}
-                            className="w-full p-2 border rounded"
-                            required
+                            type="datetime-local"
+                            className={`w-full p-2 border rounded ${errors.startTime ? '!border-red-500' : 'border-gray-300'}`}
+                            {...register("startTime", { required: "Vui lòng nhập giờ bắt đầu" })}
+                            onChange={(e) => {
+                                setValue("startTime", e.target.value);
+                                handleChange("startTime");
+                            }}
                         />
+                        {errors.startTime && <p className="text-red-700">{errors.startTime.message}</p>}
                     </div>
+
                     <div className="mb-4">
-                        <label className="block text-sm font-medium">Giờ kết thúc</label>
+                        <label className="block text-sm font-medium mb-2">Giờ kết thúc</label>
                         <input
-                            type="timestamp"
-                            name="arrival"
-                            value={formData.arrival}
-                            onChange={handleChange}
-                            className="w-full p-2 border rounded"
-                            required
+                            type="datetime-local"
+                            className={`w-full p-2 border rounded ${errors.endTime ? '!border-red-500' : 'border-gray-300'}`}
+                            {...register("endTime", { required: "Vui lòng nhập giờ kết thúc" })}
+                            onChange={(e) => {
+                                setValue("endTime", e.target.value);
+                                handleChange("endTime");
+                            }}
                         />
+                        {errors.endTime && <p className="text-red-700">{errors.endTime.message}</p>}
                     </div>
+
                     <div className="mb-4">
-                        <label className="block text-sm font-medium">Giá vé</label>
+                        <label className="block text-sm font-medium mb-2">Giá vé (vnd)</label>
                         <input
-                            type="text"
-                            name="price"
-                            value={formatPrice(formData.price)}
-                            onChange={handleChange}
-                            className="w-full p-2 border rounded"
-                            required
+                            type="number"
+                            className={`w-full p-2 border rounded ${errors.ticketPrice ? '!border-red-500' : 'border-gray-300'}`}
+                            {...register("ticketPrice", {
+                                required: "Vui lòng nhập giá vé",
+                                min: { value: 0, message: "Giá vé không được âm" },
+                                pattern: {
+                                    value: /^[0-9]*$/,
+                                    message: "Vui lòng nhập số hợp lệ"
+                                }
+                            })}
+                            onChange={(e) => {
+                                setValue("ticketPrice", e.target.value);
+                                handleChange("ticketPrice");
+                            }}
                         />
+                        {errors.ticketPrice && <p className="text-red-700">{errors.ticketPrice.message}</p>}
                     </div>
-                    <button type="submit" className="px-4 py-2 bg-[#073272] text-white rounded">
-                        Cập nhật tuyến xe</button>
+
+                    <button 
+                        type="submit" 
+                        className="px-4 py-2 bg-[#073272] text-white rounded"
+                        disabled={Object.keys(errors).length > 0} 
+                    >
+                        Cập nhật tuyến xe
+                    </button>
                 </form>
             </div>
-            </div>
-       
-        );
+        </div>
+    );
 }
+
 export default BusRoutesEdit;
--- src/pages/admin/busRoutes/getAll/index.jsx
+++ src/pages/admin/busRoutes/getAll/index.jsx
@@ -1,58 +1,78 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import FormDelete from "../../../../components/formDelete";
 
 const busRouteData = [
-    { id: "1", route: "Hà Nội-TP. HCM",departure:"13h30", arrival: "23h30", price: "120" },
-    { id: "2", route: "Hà Nội-TP. HCM",departure:"13h30", arrival: "23h30", price: "1800000" },
+    { id: "1", routes: "Hà Nội-TP. HCM", departure: "13h30", arrival: "23h30", price: "120" },
+    { id: "2", routes: "Hà Nội-TP. HCM", departure: "13h30", arrival: "23h30", price: "1800000" },
 ];
 
-function BusRoutesGetAll(){
+function BusRoutesGetAll() {
     const formatPrice = (price) => {
-        return price.toLocaleString('vi-VN') + ' đ';
+        return price.toLocaleString('vi-VN') + 'vnd';
     };
-    return(
+    const [selectedRoute, setSelectedRoute] = useState(null);
+    const [routes, setRoutes] = useState(busRouteData);
+    
+    const handleDelete = () => {
+        setRoutes(routes.filter((route) => route.id !== selectedRoute.id));
+        setSelectedRoute(null);
+    };
+
+    return (
         <div className="container mx-auto p-2">
-                    <div className="bg-white p-4 shadow rounded-md">
-                        <Link
-                            to="/admin/busRoutes/create"
-                            className=" inline-block bg-[#073272] text-white px-4 py-2 rounded"
-                        >
-                            Thêm tuyến xe
-                        </Link>
-                        <table className="w-full border-collapse border border-gray-300 mt-4">
-                            <thead>
-                                <tr className="bg-gray-200">
-                                    <th className="p-2 border">Tuyến đường</th>
-                                    <th className="p-2 border">Giờ bắt đầu</th>
-                                    <th className="p-2 border">Giờ kết thúc</th>
-                                    <th className="p-2 border">Giá vé</th>
-                                    <th className="p-2 border">Hành động</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {busRouteData.map((busroute) => (
-                                    <tr key={busroute.id} className="border-b">
-                                        <td className="p-2 border">{busroute.route}</td>
-                                        <td className="p-2 border">{busroute.departure}</td>
-                                        <td className="p-2 border">{busroute.arrival}</td>
-                                        <td className="p-2 border">{formatPrice(busroute.price)}</td>
-                                        <td className="p-2 border flex gap-2">
-                                            <Link
-                                                to={`/admin/busRoutes/edit/${busroute.id}`}
-                                                className="bg-yellow-500 text-white py-2 px-3 rounded"
-                                            >
-                                                <i className="fa-solid fa-pen-to-square text-md"></i>
-                                            </Link>
-                                            <button className="bg-red-500 text-white py-2 px-3 rounded">
-                                                <i className="fa-solid fa-trash text-md"></i>
-                                            </button>
-                                        </td>
-                                     
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+            <div className="bg-white p-4 shadow rounded-md">
+                <Link
+                    to="/admin/busRoutes/create"
+                    className=" inline-block bg-[#073272] text-white px-4 py-2 rounded"
+                >
+                    Thêm tuyến xe
+                </Link>
+                <table className="w-full border-collapse border border-gray-300 mt-4">
+                    <thead>
+                        <tr className="bg-gray-200">
+                            <th className="p-2 border">Tuyến đường</th>
+                            <th className="p-2 border">Giờ bắt đầu</th>
+                            <th className="p-2 border">Giờ kết thúc</th>
+                            <th className="p-2 border">Giá vé</th>
+                            <th className="p-2 border">Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {busRouteData.map((busroute) => (
+                            <tr key={busroute.id} className="border-b">
+                                <td className="p-2 border">{busroute.routes}</td>
+                                <td className="p-2 border">{busroute.departure}</td>
+                                <td className="p-2 border">{busroute.arrival}</td>
+                                <td className="p-2 border">{formatPrice(busroute.price)}</td>
+                                <td className="p-2 border flex gap-2">
+                                    <Link
+                                        to={`/admin/busRoutes/edit/${busroute.id}`}
+                                        className="bg-yellow-500 text-white py-2 px-3 rounded"
+                                    >
+                                        <i className="fa-solid fa-pen-to-square text-md"></i>
+                                    </Link>
+                                    <button 
+                                        onClick={() => setSelectedRoute(busroute)} 
+                                        className="bg-red-500 text-white py-2 px-3 rounded"
+                                    >
+                                        <i className="fa-solid fa-trash text-md"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            {selectedRoute && (
+                <FormDelete
+                    isOpen={!!selectedRoute}
+                    onClose={() => setSelectedRoute(null)}
+                    onConfirm={handleDelete}
+                    message={`Bạn có chắc chắn muốn xóa tuyến xe "${selectedRoute.routes}" không?`}
+                />
+            )}
+        </div>
     )
 }
 export default BusRoutesGetAll;
