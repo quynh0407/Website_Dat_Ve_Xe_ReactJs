31488059faaeaa65dc1c8c69e4258eef705fd4a8
trongpc09128 <trongpc09128@gmail.com>
2025-04-01 23:08:32 +0700

    [ CLIENT - FE ] - note4.VALIDATION.index-Register
    (cherry picked from commit ec8773b2041e021b192aa430280d8dbd86e11956)

--- src/pages/admin/contact/Edit/index.jsx
+++ src/pages/admin/contact/Edit/index.jsx
@@ -35,7 +35,7 @@ function ContactEdit() {
         <div className="container mx-auto p-4">
             <div className="bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto">
                 <h3 className="text-2xl font-bold">Sửa Liên Hệ</h3>
-                <form onSubmit={handleSubmit(onSubmit)} className="p-4 border rounded-md shadow-lg">
+                <form  className="p-4 border rounded-md shadow-lg">
                     <div className="mb-4">
                         <label className="block text-sm font-medium">ID</label>
                         <input type="text" value={id} disabled className="w-full p-2 border rounded" />
@@ -98,7 +98,7 @@ function ContactEdit() {
                             <option value="Đã phản hồi">Đã phản hồi</option>
                         </select>
                     </div>
-                    <button type="submit" className="px-4 py-2 bg-[#073272] text-white rounded">
+                    <button type="submit" onClick={handleSubmit(onSubmit)} className="px-4 py-2 bg-[#073272] text-white rounded">
                         Cập nhật liên hệ
                     </button>
                 </form>
--- src/pages/client/Register/index.jsx
+++ src/pages/client/Register/index.jsx
@@ -1,36 +1,72 @@
+import { useForm } from "react-hook-form";
 function Register() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <main className="mx-auto w-full md:w-[80%] px-4 flex items-center justify-center h-screen"  >
             <div className="w-full max-w-md bg-white my-[50px] p-6 rounded-lg shadow-lg">
             <a href="/" className="text-[#043175] hover:underline hover:text-blue-800 text-sm">&larr; Quay về trang chủ</a>
-
                 <h2 className="text-2xl font-bold text-center text-[#043175]">Đăng ký</h2>
-                <form className="">
+                <form className="p-3">
                     <div>
                         <label className="block text-sm mb-1 font-bold text-[#043175]">Họ và Tên</label>
-                        <input type="text" required className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"/>
+                        <input type="text"  className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            {...register("name", {
+                                required: { value: true, message: "Họ và Tên không được để trống" }
+                            })}
+                        />
+                        {errors.name && <small className="text-red-500">{errors.name.message}</small>}
                     </div>
         
                     <div className="mt-3">
                         <label className="block text-sm mb-1 font-bold text-[#043175]">Số điện thoại</label>
-                        <input type="tel" required className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"/>
+                        <input type="tel"  className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            {...register("phone", {
+                                required: "Số điện thoại không được để trống",
+                                pattern: {
+                                    value: /^[0-9]{10}$/,
+                                    message: "Số điện thoại không hợp lệ, chỉ được nhập 10 chữ số",
+                                },
+                            })}
+                        />
+                        {errors.phone && <small className="text-red-500">{errors.phone.message}</small>}
                     </div>
         
                     <div className="mt-3">
                         <label className="block text-sm mb-1 font-bold text-[#043175]">Email</label>
-                        <input type="email" required className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"/>
+                        <input type="email"  className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            {...register("email", {
+                                required: "Email không được để trống",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "Email không hợp lệ",
+                                },
+                            })}
+                        />
+                        {errors.email && <small className="text-red-500">{errors.email.message}</small>}
                     </div>
         
                     <div className="mt-3">
                         <label className="block text-sm mb-1 font-bold text-[#043175]">Mật khẩu</label>
-                        <input type="password" required className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"/>
+                        <input type="password"  className="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            {...register("password", {
+                                required: "Mật khẩu không được để trống",
+                                minLength: {
+                                    value: 5,
+                                    message: "Mật khẩu phải ít nhất 5 ký tự",
+                                },
+                            })}
+                        />
+                        {errors.password && <small className="text-red-500">{errors.password.message}</small>}
                     </div>
+
         
-                    <button className="w-full bg-[#043175] text-white py-2 rounded-lg mt-4 hover:bg-blue-800">Đăng ký</button>
+                    <button onClick={handleSubmit(onSubmit)} type="submit" className="w-full bg-[#043175] text-white py-2 rounded-lg mt-4 hover:bg-blue-800">Đăng ký</button>
         
                     <p className="text-center text-sm mt-3">
                         Đã có tài khoản? <a href="/login" className="text-[#043175] hover:underline">Đăng nhập</a>
-                 
                     </p>
                 </form>
             </div>
